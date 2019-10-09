d5d6614f50b700567051b70f2dbdf97ba58bada7
Chris Wegscheid <cwegscheid08@outlook.com>
2019-10-09 19:41:50 -0400

    adding target blank to say_hello.js

--- style/say_hello.js
+++ style/say_hello.js
@@ -1,5 +1,5 @@
 let sayHello = function() {
 	var form = document.getElementById('content');
-	form.innerHTML = "Hey, you found me. You should come by the site. <a href=\"https://www.wegscheidwebservices.com\">WWS</a>";
+	form.innerHTML = "Hey, you found me. You should come by the site. <a target="_blank" href=\"https://www.wegscheidwebservices.com\">WWS</a>";
 };
 
